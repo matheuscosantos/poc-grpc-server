d674d7731f0a0e0c43454dd5921d514c5b12b2e7
Matheus C O Santos <oliveiramatheus77@gmail.com>
2021-03-15 09:50:50 -0300

    Adiciona POC de servidor gRPC

---
+++ src/main/kotlin/br/com/mcos/FretesGrpcServer.kt
@@ -0,0 +1,30 @@
+package br.com.mcos
+
+import io.grpc.stub.StreamObserver
+import org.slf4j.LoggerFactory
+import javax.inject.Singleton
+import kotlin.random.Random
+
+@Singleton
+class FretesGrpcServer : FretesServiceGrpc.FretesServiceImplBase() {
+
+    private val logger = LoggerFactory.getLogger(FretesGrpcServer::class.java)
+
+    override fun calculaFrete(
+        request: CalculaFreteRequest?,
+        responseObserver: StreamObserver<CalculaFreteResponse>?,
+    ) {
+        logger.info("Calculando frete para request: $request")
+
+        val response = CalculaFreteResponse.newBuilder()
+                                           .setCep(request!!.cep)
+                                           .setValor(Random.nextDouble(from = 0.0, until = 140.0))
+                                           .build()
+
+        logger.info("Frete calculado: $response")
+
+        responseObserver!!.onNext(response)
+        responseObserver.onCompleted()
+
+    }
+}
